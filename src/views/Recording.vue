e586c9627a7935ea27482a422337f4fdc228eaa8
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-09-30 01:04:13 +0100

    Add Recording Markup

---
+++ src/views/Recording.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="recording-section">
+    <div class="wrapper">
+      <figure class="recording-svgs">
+        <div class="time">4:14</div>
+        <img src="./assets/img/sound.svg" class="recording__svg-overlay" alt="record icon overlay">
+        <img src="./assets/img/030-microphone.svg" class="recording__svg-record" alt="record button">
+        <div class="recording-button">
+          <a href="/record" class="btn btn--body">Stop</a>
+        </div>
+      </figure>
+      <div class="container">
+        <div class="recording-container">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae sapiente assumenda voluptates,
+          veritatis eos sit maiores corrupti ad amet iure debitis neque quo culpa eveniet, ipsa odit odio rem
+          explicabo? Est nostrum accusamus libero, autem vero possimus magni consectetur odio unde optio esse
+          adipisci aut placeat fugiat nesciunt fugit cum suscipit sit. Perferendis amet, quia ullam accusantium
+          nulla
+          nemo, dignissimos autem iste, laborum blanditiis quae! Nam quia sed nobis est, labore consequatur quaerat
+          accusamus fugiat hic laudantium ut optio fuga eum, vel molestiae nisi minus nesciunt voluptates suscipit
+          ullam? Iure consequuntur totam rerum ipsa natus! Assumenda esse ipsum nam aut ratione repellendus
+          consectetur sed libero dignissimos et ipsam delectus consequuntur, accusamus at, quasi ab reprehenderit
+          odio natus. Illo facere omnis quam dolore saepe enim aliquam ipsam non! Quod aliquid cupiditate
+          asperiores
+          omnis, possimus rerum magnam veritatis aperiam aspernatur suscipit repellat tenetur sint facilis nesciunt
+          libero. Dicta sint reprehenderit deleniti laudantium.
+          <br>
+          <br>
+
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium aperiam magni molestiae, tempore
+          fugit dolorem iste recusandae perspiciatis ullam minima atque similique optio excepturi alias fugiat sed
+          omnis eveniet. Nobis nihil, repellendus fuga quidem ad fugit illo dolor esse necessitatibus adipisci
+          natus expedita molestias nulla alias vel earum quibusdam est?
+        </div>
+        <div class="save-button">
+          <a href="/record" class="btn btn--body">Save</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
