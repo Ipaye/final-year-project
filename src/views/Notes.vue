f6b42f004f830bf1a2ac986fe230ce89277185cd
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-09-30 01:03:23 +0100

    Add Notes Markup

---
+++ src/views/Notes.vue
@@ -0,0 +1,74 @@
+<template>
+   <section class="note-section none">
+        <div class="note-image">
+          <img src="../assets/img/Top-icon-svg.svg" class="top-note-image" alt="Note icon">
+        </div>
+        <div class="notes">
+          <div class="note">
+            <div class="note__content"></div>
+            <div class="note__footer">
+              <div class="note__footer--left">Content edited</div>
+              <div class="note__footer--right">logo logo logo </div>
+            </div>
+          </div>
+
+          <div class="note">
+            <div class="note__content"></div>
+            <div class="note__footer">
+              <div class="note__footer--left">Content edited</div>
+              <div class="note__footer--right">logo logo logo </div>
+            </div>
+          </div>
+
+          <div class="note">
+            <div class="note__content"></div>
+            <div class="note__footer">
+              <div class="note__footer--left">Content edited</div>
+              <div class="note__footer--right">logo logo logo </div>
+            </div>
+          </div>
+
+          <div class="note">
+            <div class="note__content"></div>
+            <div class="note__footer">
+              <div class="note__footer--left">Content edited</div>
+              <div class="note__footer--right">logo logo logo </div>
+            </div>
+          </div>
+
+          <div class="note">
+            <div class="note__content"></div>
+            <div class="note__footer">
+              <div class="note__footer--left">Content edited</div>
+              <div class="note__footer--right">logo logo logo </div>
+            </div>
+          </div>
+
+          <div class="note">
+            <div class="note__content"></div>
+            <div class="note__footer">
+              <div class="note__footer--left">Content edited</div>
+              <div class="note__footer--right">logo logo logo </div>
+            </div>
+          </div>
+
+          <div class="note">
+            <div class="note__content"></div>
+            <div class="note__footer">
+              <div class="note__footer--left">Content edited</div>
+              <div class="note__footer--right">logo logo logo </div>
+            </div>
+          </div>
+
+          <div class="note">
+            <div class="note__content"></div>
+            <div class="note__footer">
+              <div class="note__footer--left">Content edited</div>
+              <div class="note__footer--right">logo logo logo </div>
+            </div>
+          </div>
+
+
+        </div>
+      </section>
+</template>
