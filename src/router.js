37049ffa56c9e99a430cb265290ca1d354615862
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-09-30 01:31:53 +0100

    update link for all available pages --no Js plugged in

--- src/router.js
+++ src/router.js
@@ -1,23 +1,48 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Notes from './views/Notes.vue';
+import Record from './views/Record.vue';
+import Recording from './views/Recording.vue';
+import Settings from './views/Settings.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
-})
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: Home
+		},
+		{
+			path: '/notes',
+			name: 'notes',
+			component: Notes
+		},
+		{
+			path: '/settings',
+			name: 'settings',
+			component: Settings
+		},
+		{
+			path: '/record',
+			name: 'record',
+			component: Record
+		},
+		{
+			path: '/recording',
+			name: 'recording',
+			component: Recording
+		},
+		{
+			path: '/credit',
+			name: 'credit',
+			// route level code-splitting
+			// this generates a separate chunk (about.[hash].js) for this route
+			// which is lazy-loaded when the route is visited.
+			component: () =>
+				import(/* webpackChunkName: "about" */ './views/Credit.vue')
+		}
+	]
+});
