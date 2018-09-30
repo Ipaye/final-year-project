dec4f114a56ba15028647d265140b6ead673355d
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-09-30 01:00:32 +0100

    linked scss folder created in Assets Dir

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
+     <aside id="sidebar">
+      <div class="sidebar__hamburger">
+        <img src="./assets/img/hamburger.svg" alt="humburger icon" class="icon icon--sidebar">
+      </div>
+      <div class="sidebar__brand">
+        <img src="./assets/img/speeco-logo.png" alt="speeco logo" class="brand-logo">
+      </div>
+      <div>
+        <div class="sidebar__navigation">
+          <li href="#" class="sidebar__navigation-item">
+            <img src="./assets/img/edit.svg" class="icon icon--documentation" alt="documentation icon">
+            <p>Documentation</p>
+          </li>
+          <li href="#" class="sidebar__navigation-item">
+            <img src="./assets/img/file.svg" class="icon icon--setting" alt="file icon">
+            <p>Notes</p>
+          </li>
+          <li href="#" class="sidebar__navigation-item">
+            <img src="./assets/img/user.svg" class="icon icon--credits" alt="credits icon">
+            <p>Credits</p>
+          </li>
+        </div>
+      </div>
+      <div class="sidebar__btn">
+        <a href="#" class="btn btn--sidebar">Record</a>
+      </div>
+
+      <div class="sidebar__setting">
+        <img src="./assets/img/cogwheel-outline.svg" class="icon icon--setting" alt="setting icon">
+        <p>Setting</p>
+      </div>
+
+    </aside>
+     <main id="mainContent">  
+    </main>
+    <img src="/img/UnderBucket.svg" class="buttomBucket none" alt="floating bucket">
+
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
