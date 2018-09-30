f08009068cf2b7943b41197f6fe996977993b9c0
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-09-30 01:03:38 +0100

    Add Record Markup

---
+++ src/views/Record.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="record-section" style="display:none">
+    <figure class="record-svgs">
+      <img src="./assets/img/sound.svg" class="record__svg-overlay" alt="record icon overlay">
+      <img src="./assets/img/mic-start.svg" class="record__svg-record" alt="record button">
+    </figure>
+    <div class="record-button">
+      <a href="/record" class="btn btn--body">start</a>
+    </div>
+  </section>
+</template>
