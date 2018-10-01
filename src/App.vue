37c0287acc580c06e3b4865cc0da0cbb602916b8
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-10-01 19:29:56 +0100

    updated Markup to Add vue routing

--- src/App.vue
+++ src/App.vue
@@ -2,38 +2,48 @@
   <div id="app">
      <aside id="sidebar" :class="{active: active}">
       <div class="sidebar__hamburger">
+        <li class="sidebar__navigation-item">
         <img src="./assets/img/hamburger.svg" alt="humburger icon" @click="active = !active" class="icon icon--sidebar">
+        </li>
       </div>
       <div class="sidebar__brand">
+        <router-link to="/">
         <img src="./assets/img/speeco-logo.png" alt="speeco logo" class="brand-logo">
+        </router-link>
       </div>
       <div>
         <div class="sidebar__navigation">
-          <li href="#" class="sidebar__navigation-item">
-            <img src="./assets/img/edit.svg" class="icon icon--documentation" alt="documentation icon">
-            <p class="sidebar__navigation--text">Documentation</p>
+          <li class="sidebar__navigation-item">
+            <router-link href="/documentation" to="/documentation">
+              <img src="./assets/img/edit.svg" class="icon icon--documentation" alt="documentation icon">
+              <p class="sidebar__navigation--text">Documentation</p></router-link>            
           </li>
-          <li href="#" class="sidebar__navigation-item">
-            <img src="./assets/img/file.svg" class="icon icon--setting" alt="file icon">
-            <p class="sidebar__navigation--text">Notes</p>
+          <li class="sidebar__navigation-item">
+            <router-link to="/notes" href="/notes">
+              <img src="./assets/img/file.svg" class="icon icon--setting" alt="file icon">
+             <p class="sidebar__navigation--text">Notes</p>
+            </router-link>            
           </li>
-          <li href="#" class="sidebar__navigation-item">
-            <img src="./assets/img/user.svg" class="icon icon--credits" alt="credits icon">
+          <li class="sidebar__navigation-item">
+            <router-link to="credits" href="/credits">
+              <img src="./assets/img/user.svg" class="icon icon--credits" alt="credits icon">
             <p class="sidebar__navigation--text">Credits</p>
+            </router-link>
           </li>
         </div>
       </div>
       <div class="sidebar__btn">
-        <a href="#" class="btn btn--sidebar">Record</a>
+        <router-link to="/record" href="#" class="btn btn--sidebar">Record</router-link>
       </div>
 
       <div class="sidebar__setting">
-        <img src="./assets/img/cogwheel-outline.svg" class="icon icon--setting" alt="setting icon">
+        <img src="./assets/img/setting-icon.svg" class="icon icon--setting" alt="setting icon">
         <p class="sidebar__navigation--text">Setting</p>
       </div>
 
     </aside>
      <main id="mainContent">  
+
     </main>
     <img src="/img/UnderBucket.svg" class="buttomBucket none" alt="floating bucket">
 
