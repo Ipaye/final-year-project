90dab813974d6dfece098a1add115b8e41d2bd50
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-10-03 18:24:55 +0100

    implemented the timer part of recording

--- src/views/Recording.vue
+++ src/views/Recording.vue
@@ -3,36 +3,16 @@
   <section class="recording-section">
     <div class="wrapper">
       <figure class="recording-svgs">
-        <div class="time">4:14</div>
+        <div class="time">{{padNumber(minute)}}:{{padNumber(seconds)}}</div>
         <img src="../assets/img/sound.svg" class="recording__svg-overlay" alt="record icon overlay">
         <img src="../assets/img/030-microphone.svg" class="recording__svg-record" alt="record button">
         <div class="recording-button">
-          <a href="/record" class="btn btn--body">Stop</a>
+          <a class="btn btn--body" @click="switchState">{{state}}</a>
         </div>
       </figure>
       <div class="container">
-        <div class="recording-container">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae sapiente assumenda voluptates,
-          veritatis eos sit maiores corrupti ad amet iure debitis neque quo culpa eveniet, ipsa odit odio rem
-          explicabo? Est nostrum accusamus libero, autem vero possimus magni consectetur odio unde optio esse
-          adipisci aut placeat fugiat nesciunt fugit cum suscipit sit. Perferendis amet, quia ullam accusantium
-          nulla
-          nemo, dignissimos autem iste, laborum blanditiis quae! Nam quia sed nobis est, labore consequatur quaerat
-          accusamus fugiat hic laudantium ut optio fuga eum, vel molestiae nisi minus nesciunt voluptates suscipit
-          ullam? Iure consequuntur totam rerum ipsa natus! Assumenda esse ipsum nam aut ratione repellendus
-          consectetur sed libero dignissimos et ipsam delectus consequuntur, accusamus at, quasi ab reprehenderit
-          odio natus. Illo facere omnis quam dolore saepe enim aliquam ipsam non! Quod aliquid cupiditate
-          asperiores
-          omnis, possimus rerum magnam veritatis aperiam aspernatur suscipit repellat tenetur sint facilis nesciunt
-          libero. Dicta sint reprehenderit deleniti laudantium.
-          <br>
-          <br>
-
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium aperiam magni molestiae, tempore
-          fugit dolorem iste recusandae perspiciatis ullam minima atque similique optio excepturi alias fugiat sed
-          omnis eveniet. Nobis nihil, repellendus fuga quidem ad fugit illo dolor esse necessitatibus adipisci
-          natus expedita molestias nulla alias vel earum quibusdam est?
-        </div>
+        <textarea class="recording-container" cols="30" rows="10"> </textarea>
+        
         <div class="save-button">
           <router-link to="/notes" href="/notes" class="btn btn--body">Save</router-link>
         </div>
@@ -41,3 +21,51 @@
   </section>
   </main>
 </template>
+
+<script>
+export default {
+	name: 'recording',
+	data() {
+		return {
+			message: 'Hello world',
+			toggle: false,
+			state: 'Start',
+			minute: 0,
+			seconds: 0,
+			counter: 50,
+			interval: null
+		};
+	},
+	methods: {
+		switchState() {
+			if (this.toggle == false) {
+				this.toggle = !this.toggle;
+				this.state = 'Stop';
+				this.startCounting();
+			} else {
+				this.state = 'start';
+				this.toggle = !this.toggle;
+				clearInterval(this.interval);
+			}
+		},
+		padNumber(number) {
+			if (number < 10) {
+				return '0' + number;
+			} else {
+				return number;
+			}
+		},
+		startCounting() {
+			this.interval = setInterval(() => {
+				const count = ++this.counter;
+				if (this.counter >= 60) {
+					this.seconds = this.counter % 60;
+					this.minute = parseInt(count / 60);
+				} else {
+					this.seconds = count;
+				}
+			}, 1000);
+		}
+	}
+};
+</script>
