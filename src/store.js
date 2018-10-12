e617a68022fcdc3601e9afd3d5015393a960a820
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-10-12 05:16:26 +0100

    Implemented state manangement for saved notes

--- src/store.js
+++ src/store.js
@@ -1,16 +1,34 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
-})
+	state: {
+		notes: [
+			'Hello world',
+			'Hello world',
+			'Hello world',
+			'Hello world',
+			'Hello world',
+			'Hello world',
+			'Hello world',
+			'Hello world',
+			'Hello world',
+			'Hello world',
+			'Hello world'
+		]
+	},
+	getters: {
+		availableNotes(state, getters) {
+			return state.notes;
+		}
+	},
+	mutations: {
+		addNote(state, payload) {
+			console.log(payload);
+			state.notes.unshift(payload);
+		}
+	},
+	actions: {}
+});
