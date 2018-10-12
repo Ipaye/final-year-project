20795e375d726dbdcb6081a2c87ecabda19288bd
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-10-12 06:24:01 +0100

    Updates

---
+++ js/about.b6204a77.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{b7b2:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],i=n("2877"),u={},l=Object(i["a"])(u,a,s,!1,null,null,null);l.options.__file="Credit.vue";e["default"]=l.exports}}]);
+//# sourceMappingURL=about.b6204a77.js.map
