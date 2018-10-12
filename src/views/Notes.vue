812ca9abc39acc9753de79c422f6b68ced96e926
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-10-12 05:17:20 +0100

    Add created hook to fetch all notes

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -5,72 +5,34 @@
           <img src="../assets/img/Top-icon-svg.svg" class="top-note-image" alt="Note icon">
         </div>
         <div class="notes">
-          <div class="note">
-            <div class="note__content"></div>
+          <div class="note" v-for="(note, index) in notes" :key="index">
+            <div class="note__content"> {{note}} </div>
             <div class="note__footer">
               <div class="note__footer--left">Content edited</div>
               <div class="note__footer--right">logo logo logo </div>
             </div>
           </div>
-
-          <div class="note">
-            <div class="note__content"></div>
-            <div class="note__footer">
-              <div class="note__footer--left">Content edited</div>
-              <div class="note__footer--right">logo logo logo </div>
-            </div>
-          </div>
-
-          <div class="note">
-            <div class="note__content"></div>
-            <div class="note__footer">
-              <div class="note__footer--left">Content edited</div>
-              <div class="note__footer--right">logo logo logo </div>
-            </div>
-          </div>
-
-          <div class="note">
-            <div class="note__content"></div>
-            <div class="note__footer">
-              <div class="note__footer--left">Content edited</div>
-              <div class="note__footer--right">logo logo logo </div>
-            </div>
-          </div>
-
-          <div class="note">
-            <div class="note__content"></div>
-            <div class="note__footer">
-              <div class="note__footer--left">Content edited</div>
-              <div class="note__footer--right">logo logo logo </div>
-            </div>
-          </div>
-
-          <div class="note">
-            <div class="note__content"></div>
-            <div class="note__footer">
-              <div class="note__footer--left">Content edited</div>
-              <div class="note__footer--right">logo logo logo </div>
-            </div>
-          </div>
-
-          <div class="note">
-            <div class="note__content"></div>
-            <div class="note__footer">
-              <div class="note__footer--left">Content edited</div>
-              <div class="note__footer--right">logo logo logo </div>
-            </div>
-          </div>
-
-          <div class="note">
-            <div class="note__content"></div>
-            <div class="note__footer">
-              <div class="note__footer--left">Content edited</div>
-              <div class="note__footer--right">logo logo logo </div>
-            </div>
-          </div>
-
-
         </div>
       </section>
     </main>
 </template>
+<script>
+import store from '@/store';
+export default {
+	name: 'note',
+	data() {
+		return {
+			notes: []
+		};
+	},
+	methods: {
+		displayNotes() {
+			this.notes = store.getters.availableNotes;
+			// return store.getters.availableNotes;
+		}
+	},
+	created: function() {
+		this.displayNotes();
+	}
+};
+</script>
